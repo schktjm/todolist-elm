89c61a9212fcf42804b10f0da99c16d61571fae1
schktjm <si2.o.mjt@gmail.com>
2019-11-03 20:46:09 +0900

    feature create

---
+++ src/Main.elm
@@ -0,0 +1,59 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- MAIN
+main =  Browser.sandbox {init = init,update = update, view = view} 
+
+-- MODEL
+
+type alias Todo =
+    {text: String}
+
+
+type alias Model = 
+    { todos: List Todo, text: String }
+
+init : Model
+init = Model  
+    [] ""
+
+
+-- UPDATE
+
+type Msg = Change String | Add 
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change val ->
+            { model | text = val }
+        Add  ->
+            { model | todos = (Todo model.text)::model.todos }
+        
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div[]
+    [ div[]
+        [ input [ placeholder "input text", value model.text, onInput Change ][]
+        , div [][ text model.text ]
+        , button [ onClick Add ][]
+        ]
+    , div[]
+        [
+            ul[]( List.map viewTodo model.todos )
+        ]
+    ]
+
+
+viewTodo : Todo -> Html Msg
+viewTodo todo =
+    li[][ text todo.text ]
+    
