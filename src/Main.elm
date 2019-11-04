97bd153403a79c93f4c2209ad1ebc99b49b7a4af
schktjm <si2.o.mjt@gmail.com>
2019-11-04 23:47:26 +0900

    create Delete function

--- src/Main.elm
+++ src/Main.elm
@@ -5,27 +5,26 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-
 -- MAIN
 main =  Browser.sandbox {init = init,update = update, view = view} 
 
 -- MODEL
 
+
 type alias Todo =
     {text: String}
-
 
 type alias Model = 
     { todos: List Todo, text: String }
 
 init : Model
 init = Model  
-    [] ""
+    [] "" 
 
 
 -- UPDATE
 
-type Msg = Change String | Add 
+type Msg = Change String | Add | Delete Int | Print String
 
 update : Msg -> Model -> Model
 update msg model =
@@ -34,6 +33,18 @@ update msg model =
             { model | text = val }
         Add  ->
             { model | todos = (Todo model.text)::model.todos, text = "" }
+        Delete idx ->
+            {model | todos = 
+                model.todos
+                    |> List.indexedMap Tuple.pair
+                    |> List.filter (\(i, _) -> i /= idx)
+                    |> List.map Tuple.second
+            }
+        Print str ->
+            let
+                _ = Debug.log "print" str
+            in
+                model
         
 
 -- VIEW
@@ -47,12 +58,14 @@ view model =
         ]
     , div[]
         [
-            ul[]( List.map viewTodo model.todos )
+            ul[](List.indexedMap viewTodo  model.todos )
         ]
     ]
 
 
-viewTodo : Todo -> Html Msg
-viewTodo todo =
-    li[][ text todo.text ]
+viewTodo : Int -> Todo -> Html Msg
+viewTodo num todo =
+    li[]
+    [ text todo.text
+    , button [ onClick (Delete num) ][text "delete"] ]
     
