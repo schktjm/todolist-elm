943375b51fd7fc909676826671aeffe7562fa615
schktjm <si2.o.mjt@gmail.com>
2019-11-15 21:05:55 +0900

    :tada:

--- src/Main.elm
+++ src/Main.elm
@@ -15,16 +15,16 @@ type alias Todo =
     {text: String}
 
 type alias Model = 
-    { todos: List Todo, text: String, updateText: String, isFixing: Bool }
+    { todos: List Todo, text: String, updateIdx: Int, updateText: String, isFixing: Bool }
 
 init : Model
 init = Model  
-    [] "" "" False
+    [] "" 0 "" False
 
 
 -- UPDATE
 
-type Msg = ChangeText String | ChangeUpdateText String| Add | Delete Int | Update | Print String
+type Msg = ChangeText String | ChangeUpdateText String | UpdateNthItem | Add | Delete Int | Print String | ChangeFixFlg | ChangeUpdateIdx Int
 
 update : Msg -> Model -> Model
 update msg model =
@@ -33,6 +33,19 @@ update msg model =
             { model | text = val }
         ChangeUpdateText str ->
             { model | updateText = str}
+        UpdateNthItem ->
+            {model | isFixing = False, updateText = "", todos =
+                model.todos
+                    |> List.indexedMap Tuple.pair
+                    |> Debug.log "before"
+                    |> List.map (\(i, x) -> 
+                        if model.updateIdx == i then
+                            Todo model.updateText 
+                        else
+                            x
+                        )
+                    |> Debug.log "after"
+            }
         Add  ->
             { model | todos = (Todo model.text)::model.todos, text = "" }
         Delete idx ->
@@ -42,14 +55,15 @@ update msg model =
                     |> List.filter (\(i, _) -> i /= idx)
                     |> List.map Tuple.second
             }
-        Update -> 
-            {model | isFixing = True}
         Print str ->
             let
                 _ = Debug.log "print" str
             in
                 model
-        
+        ChangeFixFlg ->
+            {model | isFixing = not model.isFixing}
+        ChangeUpdateIdx num -> 
+            {model | updateIdx = num, isFixing = True}
 
 -- VIEW
 
@@ -72,7 +86,7 @@ viewTodo : Int -> Todo -> Html Msg
 viewTodo num todo =
     li[]
     [ text todo.text
-    , button [ onClick Update ][text "fix"]
+    , button [ onClick (ChangeUpdateIdx num) ][text "fix"]
     , button [ onClick (Delete num) ][text "delete"] ]
     
 viewUpdateArea : Model -> Html Msg
@@ -80,8 +94,8 @@ viewUpdateArea model =
     if model.isFixing then
         div[]
         [ input [ placeholder "update text", value model.updateText, onInput ChangeUpdateText][]
-        , button [][text "update"]
+        , button [onClick UpdateNthItem ][text "update"]
+        , button [ onClick ChangeFixFlg][text "x"]
         ]
     else
         div[][ text "not fixing"]
-
